af88183197a0afdb01a61d3c25e343bf0c85752f
Dan Milliron <demillir@gmail.com>
2012-01-21 17:53:14 -0800

    Don't show the encrypted password on the users#index page

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -25,8 +25,6 @@ describe "users/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Username".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Password Digest".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => false.to_s, :count => 4
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => true.to_s, :count => 2
