55610e2afa9f20b94883220c0f3075aacd8016a7
Dan Milliron <demillir@gmail.com>
2012-01-21 21:27:02 -0800

    Test suite update

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -25,8 +25,8 @@ describe "users/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Username".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => false.to_s, :count => 4
+    assert_select "tr>td", :text => table_boolean(false).to_s, :count => 4
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => true.to_s, :count => 2
+    assert_select "tr>td", :text => table_boolean(true).to_s, :count => 2
   end
 end
