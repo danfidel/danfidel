0eb78ddf3d43b31b963c43ed54a730caab4de1fe
Dan Milliron <demillir@gmail.com>
2012-01-21 17:44:17 -0800

    We don't require a password to be entered on the edit page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, :presence => true, :uniqueness => true
-  validates :password, :presence => true
+  validates :password, :presence => true, :on => :create
 end
