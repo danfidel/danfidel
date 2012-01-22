d374875eca65252b07d3e71928c7c14eb3d3b3f1
Dan Milliron <demillir@gmail.com>
2012-01-21 16:34:28 -0800

    Scaffolded the User resource

---
+++ spec/views/users/edit.html.haml_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "users/edit" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :username => "MyString",
+      :password_digest => "MyString",
+      :producer => false,
+      :director => false,
+      :actor => false
+    ))
+  end
+
+  it "renders the edit user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => users_path(@user), :method => "post" do
+      assert_select "input#user_username", :name => "user[username]"
+      assert_select "input#user_password_digest", :name => "user[password_digest]"
+      assert_select "input#user_producer", :name => "user[producer]"
+      assert_select "input#user_director", :name => "user[director]"
+      assert_select "input#user_actor", :name => "user[actor]"
+    end
+  end
+end
---
+++ spec/views/users/index.html.haml_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "users/index" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :username => "Username",
+        :password_digest => "Password Digest",
+        :producer => true,
+        :director => false,
+        :actor => false
+      ),
+      stub_model(User,
+        :username => "Username",
+        :password_digest => "Password Digest",
+        :producer => false,
+        :director => true,
+        :actor => false
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Username".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Password Digest".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => false.to_s, :count => 4
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => true.to_s, :count => 2
+  end
+end
