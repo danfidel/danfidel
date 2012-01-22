db10f36404a03c3d304a88ac5dc140d956056398
Dan Milliron <demillir@gmail.com>
2012-01-21 17:52:04 -0800

    The users#edit page now collects a cleartext password and confirmation

--- spec/views/users/edit.html.haml_spec.rb
+++ spec/views/users/edit.html.haml_spec.rb
@@ -16,7 +16,8 @@ describe "users/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => users_path(@user), :method => "post" do
-      assert_select "input#user_password_digest", :name => "user[password_digest]"
+      assert_select "input#user_password", :name => "user[password]"
+      assert_select "input#user_password_confirmation", :name => "user[password_confirmation]"
       assert_select "input#user_producer", :name => "user[producer]"
       assert_select "input#user_director", :name => "user[director]"
       assert_select "input#user_actor", :name => "user[actor]"
