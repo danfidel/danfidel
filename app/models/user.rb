5402883fe53156cac67997272955483cc1ad2402
Dan Milliron <demillir@gmail.com>
2012-01-21 23:30:14 -0800

    Added the User#security_role method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
 
   validates :username, :presence => true, :uniqueness => true
   validates :password, :presence => true, :on => :create
+
+  # Returns an array of symbols for this user's security roles, e.g. [:producer, :actor].
+  # Returns an empty array if this user has no security roles.
+  def security_roles
+    [:producer, :director, :actor].find_all { |role| self.send role }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,36 @@ describe User do
   it "should be valid from the factory" do
     user.should be_valid
   end
+
+  describe "#security_roles" do
+    it "should return an array of the user's roles" do
+      dut = Factory(:user, :producer => false, :director => false, :actor => false)
+      Set[*dut.security_roles].should == Set[]
+
+      dut = Factory(:user, :producer => false, :director => false, :actor => true)
+      Set[*dut.security_roles].should == Set[:actor]
+
+      dut = Factory(:user, :producer => false, :director => true,  :actor => false)
+      Set[*dut.security_roles].should == Set[:director]
+
+      dut = Factory(:user, :producer => false, :director => true,  :actor => true)
+      Set[*dut.security_roles].should == Set[:director, :actor]
+
+      dut = Factory(:user, :producer => true,  :director => false, :actor => false)
+      Set[*dut.security_roles].should == Set[:producer]
+
+      dut = Factory(:user, :producer => true,  :director => false, :actor => true)
+      Set[*dut.security_roles].should == Set[:producer, :actor]
+
+      dut = Factory(:user, :producer => true,  :director => true,  :actor => false)
+      Set[*dut.security_roles].should == Set[:producer, :director]
+
+      dut = Factory(:user, :producer => true,  :director => true,  :actor => true)
+      Set[*dut.security_roles].should == Set[:producer, :director, :actor]
+    end
+
+    it "should return an empty array if the user has no roles" do
+      Factory(:user, :producer => false, :director => false, :actor => false).security_roles.should be_empty
+    end
+  end
 end
